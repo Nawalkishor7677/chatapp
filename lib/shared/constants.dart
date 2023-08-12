66c6cbb4dd3305b97522572d2438e0ad11e5b3f2
nawal kishor <nawalkishore7677@gmail.comm>
2023-08-12 07:20:57 +0530

    first commit

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
+
 class Constants{
   static String appId="1:969217091608:android:94c5368cd34f5c7f9c3b6d";
   static String apiKey="AIzaSyBH0Xkp6eDnM1Rhxy7m0bkSwEmwTr1KyxE";
   static String messagingSenderId="969217091608";
   static String projectId="chatappflutter-e691f";
- final primaryColor=Color.fromARGB(255, 227, 111, 44);
+ final primaryColor=Color.fromARGB(255, 237, 105, 29);
 }
